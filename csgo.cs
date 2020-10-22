a92aed7b990919373eb3dbe2697ac7e9637b58c3
SirCoDre <66209452+SirCoDre@users.noreply.github.com>
2020-10-22 19:47:16 +0300

    Add files via upload

--- csgo.cs
+++ csgo.cs
@@ -1,6 +1,6 @@
 using System;
 
-// 2020-09-10 09:19:42.187048900 UTC
+// 2020-10-22 16:45:53.275064900 UTC
 
 namespace hazedumper
 {
@@ -24,17 +24,17 @@ namespace hazedumper
     }
     public static class signatures
     {
-        public const Int32 dwClientState = 0x589DD4;
+        public const Int32 dwClientState = 0x58CFE4;
         public const Int32 dwClientState_MaxPlayer = 0x388;
         public const Int32 dwClientState_State = 0x108;
-        public const Int32 dwClientState_ViewAngles = 0x4D88;
-        public const Int32 dwEntityList = 0x4D4F1FC;
-        public const Int32 dwForceAttack = 0x3180754;
-        public const Int32 dwGetAllClasses = 0xD6123C;
-        public const Int32 dwGlobalVars = 0x589AD8;
-        public const Int32 dwLocalPlayer = 0xD3AC5C;
-        public const Int32 dwRadarBase = 0x5183F6C;
-        public const Int32 dwViewMatrix = 0x4D40B44;
+        public const Int32 dwClientState_ViewAngles = 0x4D90;
+        public const Int32 dwEntityList = 0x4D523AC;
+        public const Int32 dwForceAttack = 0x3183930;
+        public const Int32 dwGetAllClasses = 0xD643A4;
+        public const Int32 dwGlobalVars = 0x58CCE8;
+        public const Int32 dwLocalPlayer = 0xD3DD14;
+        public const Int32 dwRadarBase = 0x518710C;
+        public const Int32 dwViewMatrix = 0x4D43CC4;
         public const Int32 m_bDormant = 0xED;
     }
 } // namespace hazedumper
